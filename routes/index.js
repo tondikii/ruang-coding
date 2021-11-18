b03df7944848c70986f9432daf2239997178ce49
luthfanadli <luthfan30@gmail.com>
2021-11-18 17:13:35 +0700

    mbuh

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,10 @@ const Controller = require('../controllers/controller');
 
 const router = require('express').Router();
 
-router.get('/', Controller.loginPage);
+router.get('/', Controller.landingPage)
+router.get('/login', Controller.loginPage);
 router.get('/register', Controller.register);
+router.post('/user', Controller.homeUser);
+
 
 module.exports = router;
